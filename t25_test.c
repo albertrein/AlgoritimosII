285f9b7d872e4037e3aa53342b8ba55f9a8fb649
Guilherme Reinheimer <guilhermealberto8@gmail.com>
2017-06-05 01:16:14 -0300

    Add files via upload

--- t25_test.c
+++ t25_test.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #define STRTAM 40
-#define MAXTRAB 10
+#define MAX 10
 
 struct ALUNOS {
 	char disc[STRTAM];
 	char codDISC[STRTAM];
 	char nomeAL[STRTAM];
 	char codAL[STRTAM];
-	int quantTRAB;
-	double somaTRAB;
-	double notasTRAB[3][MAXTRAB];
+	int quantTRAB;	
+	double notaTRAB[3][MAX];
+
 	double notaPROVA;
 	double pesoPROVA;
+	double somaTRAB;
 	double soma;
 
 };
@@ -28,27 +29,23 @@ struct ALUNOS leitura(){
 		printf("# Digite o nome da disciplina: \n");
 		fgets(a.disc, STRTAM, stdin);
 		strtok(a.disc, "\n");
-	}while(strlen(a.disc)<2);
+	}while(strlen(a.disc)<1);
 	
-	//le CóDIGO da disciplina
-	
+	//le CóDIGO da disciplina	
 	do{
 		printf("# Digite o codigo da disciplina: \n");
 		fgets(a.codDISC, STRTAM, stdin);
 		strtok(a.codDISC, "\n");
-	}while(strlen(a.codDISC)<2);
+	}while(strlen(a.codDISC)<1);
 
-
-	//le o nome do aluno
-	
+	//le o nome do aluno	
 	do{
 		printf("# Digite o nome Aluno: \n");
 		fgets(a.nomeAL, STRTAM, stdin);
 		strtok(a.nomeAL, "\n");
-	}while(strlen(a.nomeAL)<2);
+	}while(strlen(a.nomeAL)<1);
 
-	//le o COD do aluno
-	
+	//le o COD do aluno	
 	do{
 		printf("# Digite o codigo do aluno: \n");
 		fgets(a.codAL, STRTAM, stdin);
@@ -58,37 +55,42 @@ struct ALUNOS leitura(){
 
 	//le o numero de trabalhos (com máximo 10 notas)
 	for(i=0;i!=1;){
-		printf("Quantos Trabalhos (%i MAXIMO)\n",MAXTRAB);
+		printf("Quantos Trabalhos (MAXIMO %i)\n",MAX);
 		scanf("%i",&a.quantTRAB);
-		if (a.quantTRAB <= MAXTRAB && a.quantTRAB > 0){i=1;}
+		if (a.quantTRAB <= MAX && a.quantTRAB > 0){
+			i=1;
+		}
 	}
+	a.soma = 0;
 	//le os trabalhos e pesos
 	for(i=0;i<a.quantTRAB;i++){
 		j=0;
 		printf("# Nota do Trabalho %i: \n",cont);
-		scanf("%lf",&a.notasTRAB[j][i]);
-		x = a.notasTRAB[j][i];
-		printf("# Peso do Trabalho %i: \n",cont);
-		j++; 
-		scanf("%lf",&a.notasTRAB[j][i]);
-		somaPesos += a.notasTRAB[j][i];
-		if(somaPesos > 8){
-			a.notasTRAB[j][i] = 0.0;
-		}
-		x *= a.notasTRAB[j][i];
+		scanf("%lf",&a.notaTRAB[j][i]);
+		x = a.notaTRAB[j][i];
 		j++;
-		a.notasTRAB[j][i] = x;
+		printf("# Peso do Trabalho %i: \n",cont); 
+		scanf("%lf",&a.notaTRAB[j][i]);
+		somaPesos += a.notaTRAB[j][i];
+
+		if(somaPesos > 8){
+			a.notaTRAB[j][i] = 0;
+			somaPesos -= a.notaTRAB[j][i];
+		}
+
+		x = (x * a.notaTRAB[j][i])/10;
+		j++;
+		a.notaTRAB[j][i] = x;
 		a.soma += x;
-		cont++;
+		cont++;//conta numero do trab
 	}
-	a.somaTRAB = a.soma;
+	a.somaTRAB = a.soma;/// 			???
 	//le a nota da prova
 	printf("# Digite nota da prova: \n");
 	scanf("%lf",&a.notaPROVA);
 	a.pesoPROVA = MAXTRAB - somaPesos;
-	a.soma = a.soma + (a.notaPROVA*a.pesoPROVA);
-	a.soma /= 10;
-	
+	a.soma = a.soma + ((a.notaPROVA*a.pesoPROVA)/10);
+		
 
 	printf("\n______________________________TESTE DA MATRIZ_______________________________________\n");
 	for(i=0;i<3;i++){
@@ -139,7 +141,7 @@ int main(){
 		printf("%i trabalhos\n", aa[r].quantTRAB);
 		j=1;
 		for(i=0;i<aa[r].quantTRAB;i++){
-			printf("	TRAB %i: %.1lf (peso %.1lf) \n",j, aa[r].notasTRAB[aux1][i], aa[r].notasTRAB[aux2][i]);
+			printf("	TRAB %i: %.1lf (peso %.1lf) \n",j, aa[r].notaTRAB[aux1][i], aa[r].notaTRAB[aux2][i]);
 			j++;		
 		}
 		printf("Total pontos dos Trabalhos: %.1lf\n", aa[r].somaTRAB/10);
